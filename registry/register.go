7c933de96f5bbc5926459da08be5538ab2adb2fa
Lanre Adelowo <yo@lanre.wtf>
2018-09-28 01:24:26 +0100

    add consul registry pkg

---
+++ registry/register.go
@@ -0,0 +1,66 @@
+package registry
+
+import (
+	"net"
+
+	consul "github.com/hashicorp/consul/api"
+	"github.com/pborman/uuid"
+)
+
+type Client struct {
+	inner *consul.Client
+}
+
+func NewWithConfig(conf *consul.Config) (*Client, error) {
+	inner, err := consul.NewClient(conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		inner: inner,
+	}, nil
+}
+
+func NewWithClient(client *consul.Client) *Client {
+	return &Client{inner: client}
+}
+
+func New(addr string) (*Client, error) {
+	conf := consul.DefaultConfig()
+	conf.Address = addr
+
+	return NewWithConfig(conf)
+}
+
+// RegisterService takes a service definition and adds it to consul
+// It disregards whatever ID was given and uses a uuid internal so as to prevent clashes
+func (c *Client) RegisterService(svc *consul.AgentServiceRegistration) (string, error) {
+
+	id := uuid.New()
+	svc.ID = id
+
+	return id, c.inner.Agent().ServiceRegister(svc)
+}
+
+// Deregister removes a client from consul's service list
+func (c *Client) DeRegister(svc *consul.AgentServiceRegistration) error {
+	return c.inner.Agent().ServiceDeregister(svc.ID)
+}
+
+func ipAddr() (net.IP, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && ipnet.IP.IsGlobalUnicast() {
+			if ipnet.IP.To4() != nil || ipnet.IP.To16() != nil {
+				return ipnet.IP, nil
+			}
+		}
+	}
+
+	return nil, nil
+}
---
+++ registry/register_test.go
@@ -0,0 +1,52 @@
+// +build integration
+
+package registry
+
+import (
+	"os"
+	"testing"
+
+	consul "github.com/hashicorp/consul/api"
+	"github.com/pborman/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	CONSUL_ADDRESS = "CONSUL_ADDR"
+)
+
+func consulAddr() string {
+	return os.Getenv(CONSUL_ADDRESS)
+}
+
+func TestClient_Register(t *testing.T) {
+
+	client, err := New(consulAddr())
+	require.NoError(t, err)
+
+	svc := &consul.AgentServiceRegistration{
+		ID:   uuid.New(),
+		Port: 3000,
+		Name: "oops",
+	}
+
+	_, err = client.RegisterService(svc)
+	require.NoError(t, err)
+}
+
+func TestClient_DeRegister(t *testing.T) {
+
+	client, err := New(consulAddr())
+	require.NoError(t, err)
+
+	svc := &consul.AgentServiceRegistration{
+		ID:   uuid.New(),
+		Port: 3000,
+		Name: "oops",
+	}
+
+	id, err = client.RegisterService(svc)
+	require.NoError(t, err)
+
+	require.NoError(t, client.DeRegister(svc))
+}
